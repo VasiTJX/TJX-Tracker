4be3edec3a0cd60ecd9e42d51c2f35450b27f708
Fardin Kibria <fardin_kibria@tjxcanada.ca>
2021-11-18 19:16:50 -0500

    order-form

--- Application/FrontEndDev/javascripts/orders.js
+++ Application/FrontEndDev/javascripts/orders.js
@@ -16,7 +16,7 @@ $(document).ready(function () {
     // When clicking on the table launch the modal 
     $("#ordersTable").on("click" , (e) => {
         console.log("clicked");
-        var myModal = new bootstrap.Modal(document.getElementById('orderModal'), {
+        var myModal = new bootstrap.Modal(document.getElementById('orderTableModal'), {
             keyboard: false
           })
         myModal.show();
@@ -56,3 +56,84 @@ function generateOrders(orders , table){
     });
 }
 
+document.getElementById("search-order-button").addEventListener("click", async(evt) => {
+    evt.preventDefault();
+    let search_cat = document.querySelector("#dropdownMenuButton1").value;
+	let search_attr = document.querySelector("#productSearch").value;
+
+    switch (search_cat) {
+		case "Product ID":
+			search_cat = "product_id";
+			break;
+		case "SKU Number":
+			search_cat = "product_sku";
+			break;
+		case "Product Name":
+			search_cat = "product_name";
+			break;
+	}
+
+	if (search_cat === "product_id"){
+		let newproductSearch = [];
+		await axios.get("http://tjx-tracker.azurewebsites.net/api/products").then(({data}) => {
+			data.forEach(element => {
+				if (element[search_cat] == search_attr){
+					newproductSearch.push(element);	
+				}
+			});
+			let removal = document.getElementById("catalogue");
+			while (removal.firstChild){
+				removal.removeChild(removal.firstChild);
+			}
+			generateProducts(newproductSearch);
+		}); 	
+	} 
+});
+
+
+function generateProducts(products){
+
+	let picture = products.map((product) => {
+		let zone = document.createElement("div");
+		zone.classList.add("box", "zone");
+		zone.setAttribute("id", `${product.product_id}`);
+		document.getElementById("catalogue").appendChild(zone);
+		let name = document.createElement("h5");
+		name.innerHTML = `<strong>${product.product_name}</strong>`;
+		let img = document.createElement("img");
+		img.src = `${product.image_url}`;
+		document.getElementById(`${product.product_id}`).appendChild(img);
+		document.getElementById(`${product.product_id}`).appendChild(name);
+        let newZone = document.createElement("div");
+        newZone.setAttribute("id", "orderZone");
+        document.getElementById("catalogue").appendChild(newZone);
+        let options = document.createElement("label");
+        options.innerText = "Quantity";
+        document.getElementById("orderZone").appendChild(options);
+        let selection = document.createElement("select");
+        selection.classList.add("form-select");
+        selection.setAttribute("id", "orderA");
+        selection.innerHTML = `<option selected>Choose an amount</option>`;
+        document.getElementById("orderZone").appendChild(selection);
+        let contents = "";
+        let productQ = product.product_quantity;
+        for (var i=0; i<=productQ; i++) {
+            contents += `<option> ${i} </option>`;
+        }
+        document.getElementById("orderA").innerHTML = contents;
+
+    });
+ return picture;
+}
+
+
+
+function dropdownmenuSet(val){
+	if(val.innerHTML!=""){
+		$('#dropdownMenuButton1').val(val.innerHTML);
+		$('#dropdownMenuButton1').html(val.innerHTML);
+	} else {
+		$('#dropdownMenuButton1').val('');
+		$('#dropdownMenuButton1').html('Search using:');
+	}
+}
